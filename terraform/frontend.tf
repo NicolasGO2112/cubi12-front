d068358b73e910a7ce3fb63592b22e0eb06478d6
NicolasGO21 <trucelpro7@gmail.com>
2025-12-18 00:27:22 -0300

    feat:terraform configuration

---
+++ terraform/frontend.tf
@@ -0,0 +1,14 @@
+resource "render_web_service" "frontend" {
+  name   = var.frontend_name
+  plan   = "starter"      # 🔴 requerido por el enunciado
+  region = var.region
+
+  runtime = "docker"
+  image   = var.docker_image
+
+  env_vars = {
+    REACT_APP_API_URL = var.backend_url
+  }
+
+  auto_deploy = true
+}
